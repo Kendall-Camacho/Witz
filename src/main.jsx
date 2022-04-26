151c235933d5e212ed73a42aedcaffc2fe103af5
McFly <kencamacho19@gmail.com>
2022-04-26 09:56:58 -0600

    colorTest

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import App from "./App";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Write from "./pages/write/Write";
+import Single from "./pages/single/Single";
 import Register from "./pages/register/Register";
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -13,6 +14,7 @@ render(
                 <Route path="/" element={<App />} />
                 <Route path="home" element={<Home />} />
                 <Route path="write" element={<Write />} />
+                <Route path="single" element={<Single />} />
                 <Route path="login" element={<Login />} />
                 <Route path="register" element={<Register />} />
             </Route>

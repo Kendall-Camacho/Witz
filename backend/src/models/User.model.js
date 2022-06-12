7233ba552dac83586432a684ee3f01c3cfc165bf
novasuperior <lawrentvoli@gmail.com>
2022-06-12 13:39:52 -0600

    z

--- backend/src/models/User.model.js
+++ backend/src/models/User.model.js
@@ -14,6 +14,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  token: {
+    type: String,
+    required: true
+  },
   isAdmin: {
     type: Boolean,
     default: false
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,16 +1,17 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import Button from "../../components/Button/Button";
-import Register from "../register/Register";
 import "./login.css";
 
-export default function Login() {
+const Login = () => {
+  
   const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(userName, password);
+    console.log(userName, email, password);
   }
   
   return (
@@ -18,22 +19,45 @@ export default function Login() {
       <div className="Login">
         <span className="loginTitle">Login</span>
         <form className="loginForm" onSubmit={handleSubmit}>
+          <label>User Name</label>
+          <input
+            type="text"
+            className="loginInput"
+            placeholder="Enter your user name"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
           <label>Email</label>
-          <input type="text" className="loginInput" placeholder="Please, enter your email" />
+          <input 
+            type="email"
+            className="loginInput"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <label>Password</label>
-          <input type="password" className="loginInput" placeholder="Type your password" />
+          <input
+            type="password"
+            className="loginInput"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <Button classCSS="loginButton">
             <Link to="/">Login</Link>
           </Button>
         </form>
-
         <Button classCSS="Register">
           <Link to="/register">Register</Link>
         </Button>
       </div>
+
       <button className="loginButton">
         <Link to="/home">Register</Link>
       </button>
     </>
   );
 }
+
+
+export default Login;

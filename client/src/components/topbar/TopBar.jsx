f9da79cc6b2e00361a4fdbd88e1421a56877fa9d
McFly <kencamacho19@gmail.com>
2023-04-21 12:29:16 -0600

    add stuff

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -41,17 +41,14 @@ export default function TopBar() {
       theme.add("dark");
       topbar.remove("light");
       topbar.add("dark");
-      localStorage.setItem("theme", "dark");
     } else {
       theme.remove("dark");
       theme.add("light");
       topbar.remove("dark");
       topbar.add("light");
-      localStorage.setItem("theme", "light");
     }
   }
 
-  const theme = localStorage.getItem("theme");
   
   return (
     <div className='top'>
